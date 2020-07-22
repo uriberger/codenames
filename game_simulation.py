a405f871c510f62069cddf899098ba57f9ba6082
Uri Berger <uri.berger2@mail.huji.ac.il>
2020-07-22 15:30:36 +0300

    Improved the helpfulness functions

--- game_simulation.py
+++ game_simulation.py
@@ -7,7 +7,8 @@ import math
 import itertools
 
 MAX_BLUE_WORDS_NUM = 4
-#MAX_BLUE_WORDS_NUM = 1
+MIN_BLUE_WORDS_NUM = 4
+LAMBDA = 0.5
 
 debug = True
 def my_print(str):
@@ -24,9 +25,7 @@ def generate_game_word_sets(game_number):
     column_list = df.columns.ravel()
     all_words = df['words'].tolist()
     blue_words = [all_words[i] for i in range(len(all_words)) if df['colors'][i] == 'blue' and df['game number'][i] == game_number]
-    blue_words = ['apple','banana','apricot','watermelon','grapefruit']
     red_words = [all_words[i] for i in range(len(all_words)) if df['colors'][i] == 'red' and df['game number'][i] == game_number]
-    red_words = ['tiger','dog','bird','elephant','mouse']
     return blue_words,red_words
 
 def generate_game_tokens(game_number):
@@ -64,61 +63,63 @@ def generate_all_words_tokens():
         
     return res
 
-def get_best_blue_word_set(clue_word_token, blue_word_tokens):
+def get_best_blue_word_set(helpfulness_func, clue_word_token, blue_word_tokens):
+    # Given a clue word, get the best blue word set that this clue word refers to
     max_helpfulness = (-1)*math.inf
     best_word_token_set = None
     
-    for blue_words_num in range(1,MAX_BLUE_WORDS_NUM+1):
+    # Go over all the subsets of blue words in the restricted size
+    for blue_words_num in range(MIN_BLUE_WORDS_NUM,MAX_BLUE_WORDS_NUM+1):
         blue_word_token_sets = list(itertools.combinations(blue_word_tokens, blue_words_num))
         for blue_word_token_set in blue_word_token_sets:
             blue_word_vecs = [x.vector for x in blue_word_token_set]
-            #cur_helpfulness = helpfulness1(blue_word_vecs, clue_word_token.vector)
-            cur_helpfulness = helpfulness2(blue_word_vecs, clue_word_token.vector)
+            cur_helpfulness = helpfulness_func(blue_word_vecs, clue_word_token.vector)
             if cur_helpfulness > max_helpfulness:
                 max_helpfulness = cur_helpfulness
                 best_word_token_set = blue_word_token_set
     
     return max_helpfulness,best_word_token_set
 
-'''blue_tokens,red_tokens = generate_game_tokens(1)
-blue_vectors = [x.vector for x in blue_tokens]
-blue_words = [x.text for x in blue_tokens]
-red_vectors = [x.vector for x in red_tokens]
-all_words_tokens = generate_all_words_tokens()
-max_unharmfulness = (-1)*math.inf
-best_clue_word = None
-
-my_print('Generating svm model...')
-svm_model = generate_svm_model(blue_vectors,red_vectors)
-i = 0
-for cur_clue_word_token in all_words_tokens:
-    if cur_clue_word_token.text in blue_words:
-        continue
-    #cur_unharmfulness = distance_unharmfulness(red_vectors, cur_clue_word_token.vector)
-    cur_unharmfulness = svm_based_unharmfulness(svm_model, np.reshape(cur_clue_word_token.vector,(300,1)))
-    if cur_unharmfulness > max_unharmfulness:
-        max_unharmfulness = cur_unharmfulness
-        best_clue_word = cur_clue_word_token.text
-    i += 1
-print(best_clue_word)'''
-
-blue_tokens,_ = generate_game_tokens(1)
-blue_words = [x.text for x in blue_tokens]
-all_words_tokens = generate_all_words_tokens()
-max_helpfulness = (-1)*math.inf
-best_clue_word = None
-chosen_blue_word_tokens = None
-
-i = 0
-for cur_clue_word_token in all_words_tokens:
-    if cur_clue_word_token.text in blue_words:
-        continue
-    if cur_clue_word_token.text == 'fruit':
-        print('HER')
-    cur_helpfulness,best_word_token_set = get_best_blue_word_set(cur_clue_word_token, blue_tokens)
-    if cur_helpfulness > max_helpfulness:
-        max_helpfulness = cur_helpfulness
-        best_clue_word = cur_clue_word_token.text
-        chosen_blue_word_tokens = best_word_token_set
-    i += 1
-print(best_clue_word,str(chosen_blue_word_tokens))
+def generate_clue(game_number, helpfulness_func, use_svm_unharmfulness):
+    blue_tokens,red_tokens = generate_game_tokens(game_number)
+    blue_vectors = [x.vector for x in blue_tokens]
+    blue_words = [x.text.lower() for x in blue_tokens]
+    red_vectors = [x.vector for x in red_tokens]
+    all_words_tokens = generate_all_words_tokens()
+    
+    ''' Find the clue word and blue word set that maximizes the score, i.e.
+    lambda*helpfulness + (1-lambda)*unharmfulness '''
+    max_score = (-1)*math.inf
+    best_clue_word = None
+    chosen_blue_word_tokens = None
+    
+    # Generate the SVM model
+    if use_svm_unharmfulness:
+        my_print('Generating svm model...')
+        svm_model = generate_svm_model(blue_vectors,red_vectors)
+        
+    # Go over all the clue words, and choose the best one
+    for cur_clue_word_token in all_words_tokens:
+        if cur_clue_word_token.text.lower() in blue_words:
+            continue # It is illegal to give a word which is on the board
+        
+        # Calculate helpfulness
+        cur_helpfulness,best_word_token_set = get_best_blue_word_set(helpfulness_func, cur_clue_word_token, blue_tokens)
+        
+        # Calculate unharmfulness
+        if use_svm_unharmfulness:
+            cur_unharmfulness = svm_based_unharmfulness(svm_model, np.reshape(cur_clue_word_token.vector,(300,1)))
+        else:
+            cur_unharmfulness = distance_unharmfulness(red_vectors, cur_clue_word_token.vector)
+            
+        cur_score = LAMBDA * cur_helpfulness + (1-LAMBDA)*cur_unharmfulness
+        if cur_score > max_score:
+            max_score = cur_score
+            best_clue_word = cur_clue_word_token
+            chosen_blue_word_tokens = best_word_token_set
+            
+    print('Clue word: ' + str(best_clue_word) + ', referred blue words: ' + str(chosen_blue_word_tokens))
+    print('All the blue words: ' + str(blue_tokens))
+    print('All the red words: ' + str(red_tokens))
+    
+generate_clue(6, helpfulness1, True)
--- helpfulness.py
+++ helpfulness.py
@@ -14,14 +14,10 @@ def helpfulness1(blue_word_vecs, clue_word_vec):
     
     blue_words_average_vec = average_vec(blue_word_vecs)
     
-    return n * sp.spatial.distance.cosine(clue_word_vec, blue_words_average_vec)
-    #return n * np.linalg.norm(clue_word_vec-blue_words_average_vec)
+    return (-1) * n * sp.spatial.distance.cosine(clue_word_vec, blue_words_average_vec)
+    #return (-1) * n * np.linalg.norm(clue_word_vec-blue_words_average_vec)
     
 def helpfulness2(blue_word_vecs, clue_word_vec):
-    # This method is relevant only for multiple words
-    if len(blue_word_vecs) < 2:
-        return (-1)*math.inf
-    
     blue_words_average_vec = average_vec(blue_word_vecs)
     
     var_list = [(np.linalg.norm(clue_word_vec-x)-np.linalg.norm(clue_word_vec-blue_words_average_vec))**2 for x in blue_word_vecs]
